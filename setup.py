6e4fa112086c66e54f6713213c530f093b680854
Uladzimir Sakhonchik <33266845+zm99by@users.noreply.github.com>
2020-09-24 20:07:22 +0300

    Update setup.py
    add fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(),
     entry_points={
         "console_scripts": [
-            "snapshot = task3.task3",
+            "snapshot = task3.snapshot:main",
         ],
     },
     install_requires=[
