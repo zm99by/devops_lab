9729a0e5a5fa53811dd8a60896d396c6cd626043
Uladzimir Sakhonchik <33266845+zm99by@users.noreply.github.com>
2020-09-25 03:31:56 +0300

    Update pulls.py

--- handlers/pulls.py
+++ handlers/pulls.py
@@ -4,30 +4,35 @@ auth = 'your@gmail.com'
 passwd = 'your_pass'
 
 
+url = 'https://api.github.com/repos/alenaPy/devops_lab/pulls'
+
+
 def get_request(params):
-    return requests.get("https://api.github.com/repos/alenaPy/devops_lab/pulls", auth=(auth, passwd),
-                        params=params).json()
+    return requests.get(url, auth=(auth, passwd), params=params).json()
 
 
-def get_data(data):
+def get_d(d):
     arr = []
-    for x in range(len(data)):
-        arr.append({"num": data[x]["number"], "title": data[x]["title"], "link": data[x]["html_url"]})
+    for x in range(len(d)):
+        t = {"num": d[x]["number"], "title": d[x]["title"], "link": d[x]["html_url"]}
+        arr.append(t)
         x += 1
     return arr
 
 
 def get_pulls(state):
     if state == 'open' or state == 'closed':
-        return get_data(get_request({'per_page': 100, 'state': state}))
+        return get_d(get_request({'per_page': 100, 'state': state}))
     else:
-        data = get_request({'per_page': 100, 'state': "all"})
+        d = get_request({'per_page': 100, 'state': "all"})
         if state == 'accepted' or state == 'needs work':
             arr = []
-            for x in range(len(data)):
-                if data[x]["labels"] and data[x]["labels"][0]["name"] == state:
-                    arr.append({"num": data[x]["number"], "title": data[x]["title"], "link": data[x]["html_url"]})
+            for x in range(len(d)):
+                t = {"num": d[x]["number"], "title": d[x]["title"], "link": d[x]["html_url"]}
+                if d[x]["labels"] and d[x]["labels"][0]["name"] == state:
+                    arr.append(t)
                     x += 1
             return arr
         else:
-            return get_data(data)
+            return get_d(d)
+
